4af7770856da05a7b89bcbf91fb8e1adf1576fd3
Kenta Saito <ken77@gatedruid.com>
2014-04-01 15:32:39 +0900

    Fix ext/md.php to Paste to MS-Word.

--- ext/md.php
+++ ext/md.php
@@ -2,24 +2,34 @@
 require_once __DIR__.'/../vendor/autoload.php';
 use \Michelf\MarkdownExtra;
 $my_html = MarkdownExtra::defaultTransform($vars->contents);
+$my_html = str_replace('<table', '<TABLE border="1"', $my_html);
 ?>
 
 <style>
 .md h1, .md h2, .md h3, .md h4, .md h5, .md h6 {
-	margin-top: 0;
+	margin-bottom: 0;
 }
 .md li {
 	margin-left: 20px;
 	list-style-type: square;
 }
+.md table {
+	width: 100%;
+	height: 0;
+//	border-collapse: collapse;
+}
+.md th, .md td {
+//	border: 1px solid #000;
+	text-align: center;
+}
 </style>
 
 
-<form method="post" class="h100 md">
+<form method="post" class="h100">
 <table class="h100 w100">
 <tr><td class="h100" style="width:50%;"><textarea class="h100 w100" name="contents" onchange="changed = true;">
 <?php echo htmlspecialchars($vars->contents); ?></textarea>
-<td class="h100" style="width:50%;"><?php echo $my_html; ?>
+<td class="h100 md" style="width:50%;"><?php echo $my_html; ?>
 <tr><td><input type="submit" value="Save(S)" accesskey="s" onclick="changed = false;"><input type="button" value="Line jump(G)" accesskey="g" onclick="line_jump();">
 </table>
 </form>
